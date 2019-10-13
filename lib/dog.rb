faa74b0acea21720e09ed51dbfec78b626467365
Juan Carlo Banayo <johncban@gmail.com>
2019-10-13 18:44:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 require 'pry'
 require_relative "../config/environment.rb"
 
-class Dog 
-  
+class Dog
+
   attr_accessor :name, :breed, :id
 
   def initialize(id: nil, name:, breed:)
-    @id = id 
-    @name = name 
+    @id = id
+    @name = name
     @breed = breed
   end
+
   
