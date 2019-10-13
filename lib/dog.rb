f2772c4b1fe27ce9ff8807ff29a7ab9d5cd5c535
Juan Carlo Banayo <johncban@gmail.com>
2019-10-13 19:49:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -75,9 +75,9 @@ class Dog
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL 
-      SELECT * 
-      FROM dogs 
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
       WHERE name = ?
       LIMIT 1
     SQL
